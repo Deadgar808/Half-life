93ecbfe0d2c4ccb4daf8f8f6be6654d582d4253a
Drizzt808 <drizzt808@gmail.com>
2021-08-25 08:31:09 -0600

    Pushing up next phase of my code for the valve store

--- halflife.test.ts
+++ halflife.test.ts
@@ -1,4 +1,5 @@
 import { halflife } from "./halflife";
+import { halflifestore } from "./halflifestore";
 
 import {
   WebDriver,
@@ -12,25 +13,85 @@ const driver: WebDriver = new Builder()
   .withCapabilities(Capabilities.chrome())
   .build();
 const page = new halflife(driver);
-
+const storepage = new halflifestore(driver);
 //Boiler Plate above
 //Below we will compile our testing
 
 //Hitting the landing page logo
 //When interacting with lego in chrome, you need to use 'async' keyword
 
-//describe("Search for Piano on lego.com landing page", async () => {
+describe("Validating halflife top nav", async () => {
+  test("hitting half-life landing page", async () => {
+    await page.navigate();
 
-test("hitting half-life landing page", async () => {
-  await page.navigate();
-  await driver.sleep(3000);
-  await page.click(page.homepage);
+    let currentUrl = await driver.getCurrentUrl();
+    await page.takeScreenshot("screenshots/landingpage");
+    expect(currentUrl).toBe("https://www.half-life.com/en/alyx/");
+  });
 
-  let currentUrl = await driver.getCurrentUrl();
+  test("Hitting halflife home", async () => {
+    await page.navigate();
+    await page.click(page.halflifehome);
+    let currentUrl = await driver.getCurrentUrl();
+    await page.takeScreenshot("screenshots/homepage");
+    expect(currentUrl).toBe("https://www.half-life.com/en/home");
+  });
 
-  expect(currentUrl).toBe("https://half-life.com/en/home");
+  test("Hitting halflife nav button", async () => {
+    await page.click(page.halflife);
+    let currentUrl = await driver.getCurrentUrl();
+    await page.takeScreenshot("screenshots/halflife");
+    expect(currentUrl).toBe("https://www.half-life.com/en/halflife");
+  });
+
+  test("Hitting halflife2 nav button", async () => {
+    await page.click(page.halflife2);
+    let currentUrl = await driver.getCurrentUrl();
+    await page.takeScreenshot("screenshots/halflife2");
+    expect(currentUrl).toBe("https://www.half-life.com/en/halflife2");
+  });
+
+  test("Hitting episode1 button", async () => {
+    await page.click(page.halflifeE1);
+    let currentUrl = await driver.getCurrentUrl();
+    await page.takeScreenshot("screenshots/halflifeE1");
+    expect(currentUrl).toBe("https://www.half-life.com/en/episode1");
+  });
+
+  test("Hitting episode2 button", async () => {
+    await page.click(page.halflifeE2);
+    let currentUrl = await driver.getCurrentUrl();
+    await page.takeScreenshot("screenshots/halflifeE2");
+    expect(currentUrl).toBe("https://www.half-life.com/en/episode2");
+  });
+
+  test("Hitting halflife: alyx button", async () => {
+    await page.click(page.halflifealyx);
+    let currentUrl = await driver.getCurrentUrl();
+    await page.takeScreenshot("screenshots/halflifealyx");
+    expect(currentUrl).toBe("https://www.half-life.com/en/alyx");
+  });
+  test("Hitting halflife: alyx button", async () => {
+    await page.click(page.visitstore);
+    let currentUrl = await driver.getCurrentUrl();
+    await page.takeScreenshot("screenshots/halflifealyx");
+    expect(currentUrl).toBe(
+      "https://valvestore.forfansbyfans.com/title/half-life.html"
+    );
+  });
+  afterAll(async () => {
+    await driver.quit();
+  });
 });
 
-afterAll(async () => {
-  await driver.quit();
+describe("Testing Valvestore", async () => {
+  test("select black mesa", async () => {
+    await storepage.navigate();
+    await storepage.click(storepage.blackmesa);
+    await storepage.click(storepage.addtocart);
+  });
+
+  afterAll(async () => {
+    await driver.quit();
+  });
 });
--- halflife.ts
+++ halflife.ts
@@ -1,4 +1,5 @@
 import { By, until, WebDriver } from "selenium-webdriver";
+const fs = require("fs");
 //Imported Boiler Plate/Page Object model ^^^^^^^^^^^^^^^^^^^^^^
 
 //Here we export our class "Half-life" from our test file
@@ -11,13 +12,18 @@ export class halflife {
 
   //locator for continue button to proceed to correct Half-Life
 
-  homepage: By = By.xpath("//ul[@class='top_nav']/li[@class='alyx']//a[1]");
-  //halflifelanding: By = By.xpath("//h3[text()='Half-Life: Alyx'][1]");
-  //   By = By.xpath();
-  //   By = By.xpath();
-  //   By = By.xpath();
-  //   By = By.xpath();
-  //   By = By.xpath();
+  halflifehome: By = By.xpath("//ul[@class='top_nav']/li[@class='home']//a[1]");
+  halflife: By = By.xpath("//ul[@class='top_nav']/li[@class='halflife']//a[1]");
+  halflife2: By = By.xpath(
+    "//ul[@class='top_nav']/li[@class='halflife2 hl2']//a[1]"
+  );
+  halflifeE1: By = By.xpath(
+    "//ul[@class='top_nav']/li[@class='halflife2 ep1']//a[1]"
+  );
+  halflifeE2: By = By.xpath(
+    "//ul[@class='top_nav']/li[@class='halflife2 ep2']//a[1]"
+  );
+  halflifealyx: By = By.xpath("//ul[@class='top_nav']/li[@class='alyx']//a[1]");
   //   By = By.xpath();
   //   By = By.xpath();
   //   By = By.xpath();
@@ -30,11 +36,26 @@ export class halflife {
 
   async navigate() {
     await this.driver.get(this.url);
-    await this.driver.wait(until.elementLocated(this.homepage));
+    await this.driver.wait(until.elementLocated(this.halflifehome));
     await this.driver.wait(
-      until.elementIsVisible(await this.driver.findElement(this.homepage))
+      until.elementIsVisible(await this.driver.findElement(this.halflifehome))
     );
-    await this.driver.findElement(this.homepage).click();
+  }
+
+  async takeScreenshot(filepath: string) {
+    fs.writeFile(
+      `${filepath}.png`,
+      await this.driver.takeScreenshot(),
+      "base64",
+      (e) => {
+        if (e) console.log(e);
+        else console.log("screenshot saved successfully");
+      }
+    );
+  }
+  async sendKeys(elementBy: By, keys) {
+    await this.driver.wait(until.elementLocated(elementBy));
+    return this.driver.findElement(elementBy).sendKeys(keys);
   }
 
   async click(elementBy: By) {
